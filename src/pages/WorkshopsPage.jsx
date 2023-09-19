1fb78448488948df66915cc2e38b26653a5bb08f
Sebastián Rubio Quiroz <sebastian.rubio@iest.edu.mx>
2023-09-18 23:42:20 -0600

    Workshops Page

--- src/pages/WorkshopsPage.jsx
+++ src/pages/WorkshopsPage.jsx
@@ -1,5 +1,6 @@
-import { Typography } from '@mui/material'
+import { Grid, Typography } from '@mui/material'
 import { AppLayout } from '../layout/AppLayout'
+import Carousel from 'react-material-ui-carousel'
 
 export const WorkshopsPage = () => {
     return (
@@ -7,6 +8,55 @@ export const WorkshopsPage = () => {
             <AppLayout>
                 <Typography variant='h3' textAlign='center' className='animate__animated animate__fadeInRight'>👨🏽‍🏫 Workshops</Typography>
 
+                {/* Cafe 3.0 */}
+                <Grid container mt={2} className='animate__animated animate__fadeInLeft'>
+                    <Grid item xs={12} md={4} p={{xs: 0, md: 2}}>
+                        <Carousel animation='slide' duration='800' autoPlay>
+                            <img src='./assets/W2/1.jpg' className='carousel_img_3' style={{marginLeft: "auto", marginRight: "auto", display: "flex", justifyContent: "center"}}></img>
+                            <img src='./assets/W2/2.jpg' className='carousel_img_3' style={{marginLeft: "auto", marginRight: "auto", display: "flex", justifyContent: "center"}}></img>
+                            <img src='./assets/W2/3.jpg' className='carousel_img_3' style={{marginLeft: "auto", marginRight: "auto", display: "flex", justifyContent: "center"}}></img>
+                            <img src='./assets/W2/4.jpg' className='carousel_img_3' style={{marginLeft: "auto", marginRight: "auto", display: "flex", justifyContent: "center"}}></img>
+                            <img src='./assets/W2/5.jpg' className='carousel_img_3' style={{marginLeft: "auto", marginRight: "auto", display: "flex", justifyContent: "center"}}></img>
+                        </Carousel>
+                    </Grid>
+
+                    <Grid item xs={12} md={8} p={{xs: 0, md: 2}}>
+                        <Typography variant='h6'>September 2023</Typography>
+                        <Typography variant='h4' color='primary.blue'>Introduction to coding with Python</Typography>
+                        <Typography variant='h6'>SIM IEST Anáhuac</Typography>
+                        <br />
+                        <Typography variant='h7'>
+                            Pellentesque aliquam dui vitae sodales ultrices. Mauris a pellentesque odio. Morbi metus arcu, tempus vel blandit efficitur, tempor sit amet justo. In iaculis egestas urna eu scelerisque. Etiam vehicula semper elit vitae tristique. Nulla fermentum leo bibendum orci tincidunt, id posuere tellus semper. Aliquam rhoncus vel felis aliquet pharetra.
+                        </Typography>
+                    </Grid>
+
+                    <Grid item xs={12} m={2} textAlign='center'>
+                        <img src='./assets/vline.png' className='line'></img>
+                    </Grid>
+                </Grid>
+
+                {/* Cafe 3.0 */}
+                <Grid container mt={2} className='animate__animated animate__fadeInRight'>
+                    <Grid item xs={12} md={4} p={{xs: 0, md: 2}}>
+                        <Carousel animation='slide' duration='800' autoPlay>
+                            <img src='./assets/W1/1.jpg' className='carousel_img_3' style={{marginLeft: "auto", marginRight: "auto", display: "flex", justifyContent: "center"}}></img>
+                            <img src='./assets/W1/2.jpg' className='carousel_img_3' style={{marginLeft: "auto", marginRight: "auto", display: "flex", justifyContent: "center"}}></img>
+                            <img src='./assets/W1/3.jpg' className='carousel_img_3' style={{marginLeft: "auto", marginRight: "auto", display: "flex", justifyContent: "center"}}></img>
+                            <img src='./assets/W1/4.jpg' className='carousel_img_3' style={{marginLeft: "auto", marginRight: "auto", display: "flex", justifyContent: "center"}}></img>
+                            <img src='./assets/W1/5.jpg' className='carousel_img_3' style={{marginLeft: "auto", marginRight: "auto", display: "flex", justifyContent: "center"}}></img>
+                        </Carousel>
+                    </Grid>
+
+                    <Grid item xs={12} md={8} p={{xs: 0, md: 2}}>
+                        <Typography variant='h6'>April 2023</Typography>
+                        <Typography variant='h4' color='primary.blue'>Introduction to coding with Python</Typography>
+                        <Typography variant='h6'>Foro Proyecciones Empresariales</Typography>
+                        <br />
+                        <Typography variant='h7'>
+                            Pellentesque aliquam dui vitae sodales ultrices. Mauris a pellentesque odio. Morbi metus arcu, tempus vel blandit efficitur, tempor sit amet justo. In iaculis egestas urna eu scelerisque. Etiam vehicula semper elit vitae tristique. Nulla fermentum leo bibendum orci tincidunt, id posuere tellus semper. Aliquam rhoncus vel felis aliquet pharetra.
+                        </Typography>
+                    </Grid>
+                </Grid>
             </AppLayout>
         </>
     )
